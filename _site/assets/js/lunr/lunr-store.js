33b7c6c969a40e6d7cca4fdd6559b29493e1e733
chad.downey <chad.downey@shoreviewanalytics.com>
2018-02-21 15:18:39 -0500

    edit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Blog using Jekyll with Minimal-Mistakes Ubuntu 16.04",
-        "excerpt":"A few years ago, I had a wordpress site hosted on one of the more well known hosting sites. I decided that I wanted to update it or possibly look at one of my posts, so I open up a browser and navigated to my site and to my surprise...","categories": [],
+        "excerpt":"A few years ago, I had a wordpress site hosted on one of the more well known hosting sites. I decided that I wanted to update it or possibly look at one of my posts, so I opened up a browser and navigated to my site and to my surprise...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Blog-using-Jekyll-with-the-Minimal-Mistakes-theme-Ubuntu-16.04/",
         "teaser":null}]
