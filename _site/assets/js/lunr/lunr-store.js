f8a53a300d9599c9f1e0030b77859fa401c8978d
chad.downey <chad.downey@shoreviewanalytics.com>
2018-02-23 16:22:56 -0500

    more edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Create Jekyll Blog with Minimal Mistakes theme on Ubuntu 16.04",
         "excerpt":"Recently, I discovered that I can use Jekyll on Github to publish a blog. Creating a Jekyll blog and deploying to Github is nothing new, but the control that I have over my content and hosting is real value. As long as I work on my blog locally, and back...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Create-Jekyll-Blog-with-Minimal-Mistakes-theme-on-Ubuntu-16.04/",
+        "url": "https://shoreviewanalytics.github.io/Create-Jekyll-Blog-with-Minimal-Mistakes-theme-on-Ubuntu-16.04/",
         "teaser":null}]
