92e88488beef34a273850133414cbc15b1c2d0d1
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2018-04-16 14:07:24 -0400

    more edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Recently, I discovered Jekyll a static website and blog generator that you can publish on Github Pages.Why Jekyll? Creating a Jekyll blog and deploying to Github Pages, gives you complete control over your content. Hosting on Github is free. Because, I give Jekyll a :+1:!Here are the requirements I had...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Create-Jekyll-Blog-with-Minimal-Mistakes-theme-on-Ubuntu-16.04/",
+        "teaser":null},{
+        "title": "How to become a Data Scientist, Data Analyst, Data Engineer or Machine Learning Engineer",
+        "excerpt":"How to become one of the (The Big 4)Hello everyone! For this post I want to discuss how to work towards becoming aData Scientist, Data Analyst, Data Engineer or Machine Learning Engineer, whichfor brevity I will refer to as “The Big 4”. When I look at the number of jobpostings...","categories": [],
+        "tags": [],
+        "url": "https://shoreviewanalytics.github.io/How-To-Become-A-Data-Analyst-Data-Scientist-or-Data-Engineer/",
         "teaser":null}]
