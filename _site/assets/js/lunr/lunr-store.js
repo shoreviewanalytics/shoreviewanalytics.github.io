b8c7ed0ec0a14b2ec072c691e214f6c871cd843d
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2018-10-08 19:47:05 +0000

    more edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"How to become one of the (The Big 4)Hello everyone! For this post I want to discuss how to work towards becoming aData Scientist, Data Analyst, Data Engineer or Machine Learning Engineer, whichfor brevity I will refer to as “The Big 4”. When I look at the number of jobpostings...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/How-To-Become-A-Data-Analyst-Data-Scientist-or-Data-Engineer/",
+        "teaser":null},{
+        "title": "Spark Job Specific Logging with Logback and DataStax Enterprise Analytics",
+        "excerpt":"Spark Job Specific Logging with Logback and DataStax Enterprise AnalyticsThis post takes a look at how use Logback, the successor to Log4j with your spark application to create application specific logging. If you have spent any amount of time with Apache Spark you will notice that there is a ton...","categories": [],
+        "tags": [],
+        "url": "https://shoreviewanalytics.github.io/Spark-Job-Specific-Logging-with-Logback-and-DataStax-Enterprise-Analytics/",
         "teaser":null}]
