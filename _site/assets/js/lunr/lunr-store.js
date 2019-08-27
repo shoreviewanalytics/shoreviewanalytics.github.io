9dce828ee17ca057653efa521795165e670199aa
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2019-08-26 22:31:53 -0400

    more edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,29 +3,29 @@ var store = [{
         "excerpt":"Recently, I discovered Jekyll a static website and blog generator that let’s you create a feature rich, professional looking blog and then publish it on Github Pages. Why Jekyll? A Jekyll blog hosted on Github Pages, gives you complete control over your content. Hosting on Github Pages is free (for...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Create-Jekyll-Blog-with-Minimal-Mistakes-theme-on-Ubuntu-16.04/",
-        "teaser":null},{
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"},{
         "title": "How to become a Data Scientist, Data Analyst, Data Engineer or Machine Learning Engineer",
         "excerpt":"How to become one of the (The Big 4) Hello everyone! For this post I want to discuss how to work towards becoming a Data Scientist, Data Analyst, Data Engineer or Machine Learning Engineer, which for brevity I will refer to as “The Big 4”. When I look at the...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/How-To-Become-A-Data-Analyst-Data-Scientist-or-Data-Engineer/",
-        "teaser":null},{
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"},{
         "title": "Spark Logback Example One",
         "excerpt":"Spark Logback Example One This post takes a look at how use Logback, the successor to Log4j with your spark application to create application specific logging. If you have spent any amount of time with Apache Spark you will notice that there is a ton of logging that goes on,...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Spark-Logback-Example-One/",
-        "teaser":null},{
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"},{
         "title": "Spark Logback Example Two",
         "excerpt":"Spark Logback Example Two This post is relates to my other post on how to use Logback, the successor to Log4j with your spark application to create job specific logging when using the DataStax Enterprise (DSE) Analytics platform. Essentially everything is the same for this example, except you don’t need...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Spark-Logback-Example-Two/",
-        "teaser":null},{
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"},{
         "title": "Staticman API Dokku Deployment",
         "excerpt":"Using Dokku to deploy the Staticman API If you are using the public (GitHub hosted) version of Staticman API, you might have noticed that due to its popularity and GitHub API limits, it has become more difficult to use it as a comments engine for your blog or website. Perhaps...","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Staticman-API-Dokku-Deployment/",
-        "teaser":null},{
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"},{
         "title": "Fixing The 401 Unauthorized Message After Installing Tomcat 9 On Ubuntu 18.04",
         "excerpt":"Introduction Apache Tomcat® is an open source implementation of Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies. This guide explains how to setup Apache Tomcat® 9 on Ubuntu 18.04 and essentially follows the same steps that you can find in other posted tutorials (see reference below) ....","categories": [],
         "tags": [],
         "url": "https://shoreviewanalytics.github.io/Fixing-The-401-Unauthorized-Message-After-Installing-Tomcat9-On-Ubuntu-18.04/",
-        "teaser":null}]
+        "teaser":"https://shoreviewanalytics.github.io/assets/images/jekyll-logo-black-red-transparent.png"}]
